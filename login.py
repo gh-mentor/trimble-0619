092a7bfa3b730309cafdb44c27b5df964d52a1e7
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-06-19 11:51:55 -0400

    Update

---
+++ login.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def login(username, password):
+    conn = sqlite3.connect('my_database.db')
+    cursor = conn.cursor()
+
+    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+    cursor.execute(query)
+
+    result = cursor.fetchone()
+
+    if result:
+        return True  # Login successful
+    else:
+        return False  # Login failed
